1234956796c030d1c5469b3374cc912eea535719
Irwing <joshuairwing@gmail.com>
2023-02-08 03:06:28 -0300

    correção de PostarAcaoPresencial

--- src/main/java/negocio/PostagemNegocio.java
+++ src/main/java/negocio/PostagemNegocio.java
@@ -6,43 +6,4 @@ import main.java.repositorio.UsuarioRepositorio;
 public class PostagemNegocio {
 
 
-	
-	
-//	
-//	private UsuarioRepositorio repo;
-//	
-//	public UsuarioNegocio(UsuarioRepositorio rep) {
-//		this.repo = rep;
-//	}
-//	
-//	public boolean cadastrarUsuarioNome(Usuario u) {
-//		boolean ret = false;
-//		
-//		if (u.getNome().length() <= 50) {
-//			ret = this.repo.addUsuario(u);
-//		}		
-//		
-//		return ret;
-//	}
-//	
-//	public boolean cadastrarUsuarioIdade(Usuario u) {
-//		boolean ret = false;
-//		
-//		if (u.getIdade() <= 18) {
-//			ret = this.repo.addUsuario(u);
-//		}
-//		
-//		return ret;
-//	}
-//	
-//	public boolean cadastrarUsuarioCpf(Usuario u) {
-//		boolean ret = false;
-//		
-//		if (u.getCpf() != "" && u.getCpf() != null) {
-//			ret = this.repo.addUsuario(u);
-//		}
-//		
-//		return ret;
-//	}
-
 }
--- src/main/teste/PostarAcaoPresencial.java
+++ src/main/teste/PostarAcaoPresencial.java
@@ -8,7 +8,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class PostarAcaoPresencial {
 
@@ -22,12 +22,10 @@ public class PostarAcaoPresencial {
     @Test
     public void PostarAcaoPresencialTest() {
         //String com a  Data:
-
         String dateString = "22-12-2022"; //Data está salva em String na variável 'dateString'
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
 
         //Criação da Postagem:
-
         try {
             Date date = format.parse(dateString); //Conversão da data em formato String para Date
 
@@ -45,25 +43,27 @@ public class PostarAcaoPresencial {
     }
 
     // Teste 2: Mensagem em branco.
-
     @Test
     public void PostarAcaoPresencialTextTest() {
         String dateString = "22-12-2022";
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
         try {
             Date date = format.parse(dateString);
-
             String texto = ""; //String com o texto que será inserido, dessa vez em branco.
-            //String texto = "Ola, queria sugerir um grupo para distribuir sopa na praca para moradores de rua"; //Descomentar caso queira que o teste passe.
-
-
             if (texto.isEmpty()) {
                 throw new IllegalArgumentException("Por favor, coloque um texto apresentando e descrevendo a ação.");
             }
-            Postagem p = new Postagem(texto, date, "Praça do Derby"); //Ao invés de inserir a string diretamente, coloca-se a String 'texto' declarada acima.
+            //Ao invés de inserir a string diretamente, coloca-se a String 'texto' declarada acima:
+            Postagem p = new Postagem(texto, date, "Praça do Derby");
             PostagemRepositorio pr = new PostagemRepositorio();
             Boolean postou = pr.addPostagem(p);
             assertTrue(postou);
+        } catch (IllegalArgumentException e) {
+            if (e.getMessage().equals("Por favor, coloque um texto apresentando e descrevendo a ação.")) {
+                return;
+            } else {
+                throw e;
+            }
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
@@ -73,14 +73,19 @@ public class PostarAcaoPresencial {
     @Test
     public void PostarAcaoPresencialDataTest() {
         String dateString = "";
-        // String dateString = "22-12-2022"; //Descomentar caso queira que o teste passe.
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
         if (dateString.isEmpty()) {
-            throw new IllegalArgumentException("Por favor, insira uma data."); //Se a data estiver em branco, mostre esse erro.
+            try {
+                throw new IllegalArgumentException("Por favor, insira uma data.");
+            } catch (IllegalArgumentException e) {
+                assertEquals("Por favor, insira uma data.", e.getMessage());
+            }
+            return;
         }
-        try { //Resto do código continua igual.
+        try {
             Date date = format.parse(dateString);
-            Postagem p = new Postagem("Ola, queria sugerir um grupo para distribuir sopa na praca para moradores de rua", date, "Praça do Derby");
+            Postagem p = new Postagem("Ola, queria sugerir um grupo para distribuir sopa na " +
+                    "praca para moradores de rua", date, "Praça do Derby");
             PostagemRepositorio pr = new PostagemRepositorio();
             Boolean postou = pr.addPostagem(p);
             assertTrue(postou);
@@ -90,19 +95,17 @@ public class PostarAcaoPresencial {
     }
 
     //Teste 4: Data inválida.
-
     @Test
     public void PostarAcaoPresencialDataInvalidaTest() {
-
         //Cria uma instância da class Util.date. Guarda a data atual na variável 'DataAtual'.
         Date DataAtual = new Date();
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
-        // String dateString = "22-12-2022";
-        String dateString = "22-12-2023"; //Descomentar caso queira que o teste passe.
-
+        String dateString = "22-12-2022";
+        boolean thrown = false;
         try {
             Date postDate = format.parse(dateString);
             if (postDate.before(DataAtual)) {
+                thrown = true;
                 throw new IllegalArgumentException("Data inválida. Por favor, insira uma data posterior a hoje.");
             }
             Postagem p = new Postagem("Ola, queria sugerir um grupo para distribuir sopa na praca " +
@@ -112,11 +115,12 @@ public class PostarAcaoPresencial {
             assertTrue(postou);
         } catch (ParseException e) {
             throw new RuntimeException(e);
+        } catch (IllegalArgumentException e) {
+            assertTrue(thrown);
         }
     }
 
     //Teste 5: Local em branco
-
     @Test
     public void PostarAcaoPresencialLocalTest() {
         String dateString = "22-12-2022";
@@ -124,15 +128,16 @@ public class PostarAcaoPresencial {
         try {
             Date date = format.parse(dateString);
             String local = ""; //String do local da ação, dessa vez em branco.
-            // String local = "Praça do Derby"; //Descomentar caso queira que o teste passe.
             if (local.isEmpty()) {
                 throw new IllegalArgumentException("Por favor, insira um local para a ação presencial.");
             }
             Postagem p = new Postagem("Ola, queria sugerir um grupo para distribuir sopa na praca" +
-                    " para moradores de rua", date, local); //Ao invés de inserir a string diretamente, coloca-se a String 'local' declarada acima.
+                    " para moradores de rua", date, local);
             PostagemRepositorio pr = new PostagemRepositorio();
             Boolean postou = pr.addPostagem(p);
             assertTrue(postou);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Por favor, insira um local para a ação presencial.", e.getMessage());
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }

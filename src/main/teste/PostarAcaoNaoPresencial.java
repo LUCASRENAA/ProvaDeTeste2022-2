667a96fc8f521377a1b34dde030c32fa5fb6cbb8
Irwing <joshuairwing@gmail.com>
2023-02-09 02:14:18 -0300

    correção de PostarAcaoNaoPresencial

--- src/main/teste/PostarAcaoNaoPresencial.java
+++ src/main/teste/PostarAcaoNaoPresencial.java
@@ -8,155 +8,133 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class PostarAcaoNaoPresencial {
 
-    /* @Teste no. 1:
-     * 1. Declarar os valores que serão cadastrados na ação (Data inicial, Data final, valor e texto).
-     * 2. Converter ambas datas inicial e final para Date. Elas serão salvas em String.
-     * 3. Cria-se a postagemNaoPresencial.
-     * */
-
     //Teste 1: Postagem de Ação (Não Presencial)
-
     @Test
     public void PostarAcaoNaoPresencialTest() {
 
-        //Declaração dos valores.
-
+        //Declaração da String de data inicial e final
         String dataInicioString = "22-12-2022";
         String dataFinalString = "31-12-2022";
+
+        //Formatação da data para o formato "dd-MM-yyyy"
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
         try {
-
-            //Convertendo data inicial e final para Date:
-
+            //Conversão da data inicial e final para o tipo Date
             Date dataInicio = format.parse(dataInicioString);
             Date dataFinal = format.parse(dataFinalString);
 
-            //Criando nova postagem Nao Presencial:
+            //Criação de uma nova postagem (não presencial) com descrição, valor, data inicial e data final
+            PostagemNaoPresencial pn = new PostagemNaoPresencial("Ola, queria pedir ajuda para pagar uma " +
+                    "cirurgia do meu cachorro", "3600", dataInicio, dataFinal);
 
-            PostagemNaoPresencial pn = new PostagemNaoPresencial("Ola, queria pedir ajuda para pagar uma cirurgia do meu " +
-                    "cachorro","3600", dataInicio, dataFinal);
+            //Adicionar a postagem não presencial ao repositório
             PostagemNaoPresencialRepositorio pnr = new PostagemNaoPresencialRepositorio();
             Boolean addPostagemNaoPresencial = pnr.addPostagemNaoPresencial(pn);
+
+            //Verifica se a postagem foi adicionada com sucesso ao repositório
             assertTrue(addPostagemNaoPresencial);
         } catch (ParseException e) {
+            //Lança uma exceção de runtime se houver erro na conversão de data
             throw new RuntimeException(e);
         }
     }
 
     //Teste 2: Postagem de Ação Não Presencial (Sem Texto)
-
     @Test
     public void PostarAcaoNaoPresencialTextoTest() {
-
-        //Declaração dos valores.
-
+        // Declaração das variáveis "dataInicioString" e "dataFinalString", com as datas inicial e final da postagem.
         String dataInicioString = "22-12-2022";
         String dataFinalString = "31-12-2022";
+        // Declaração do formato de data utilizado nas variáveis acima.
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
-
+        // Declaração da variável "texto" que representa o texto da postagem.
         String texto = ""; //String com o texto que será inserido, dessa vez em branco.
 
-        //Descomentar caso queira que o teste passe:
-        //String texto = "Ola, queria pedir ajuda para pagar uma cirurgia do meu cachorro";
-
         try {
-
-            //Convertendo data inicial e final para Date:
-
+            // Conversão das datas de "dataInicioString" e "dataFinalString" para o tipo "Date".
             Date dataInicio = format.parse(dataInicioString);
             Date dataFinal = format.parse(dataFinalString);
 
-            //Checando se tem algo em 'texto':
-
+            // Verificação se a variável "texto" está vazia.
             if (texto.isEmpty()) {
+                // Lançamento de exceção caso a variável esteja vazia.
                 throw new IllegalArgumentException("Por favor, coloque um texto apresentando e descrevendo a ação.");
             }
 
-            //Criando nova postagem Nao Presencial:
-
-            PostagemNaoPresencial pn = new PostagemNaoPresencial(texto,"3600", dataInicio, dataFinal);
+            // Criação de uma nova postagem do tipo "PostagemNaoPresencial".
+            PostagemNaoPresencial pn = new PostagemNaoPresencial(texto, "3600", dataInicio, dataFinal);
+            // Criação de um objeto "pnr" do tipo "PostagemNaoPresencialRepositorio".
             PostagemNaoPresencialRepositorio pnr = new PostagemNaoPresencialRepositorio();
+            // Adição da postagem ao repositório.
             Boolean addPostagemNaoPresencial = pnr.addPostagemNaoPresencial(pn);
+            // Verificação se a adição foi realizada com sucesso.
             assertTrue(addPostagemNaoPresencial);
         } catch (ParseException e) {
+            // Tratamento da exceção "ParseException".
             throw new RuntimeException(e);
+        } catch (IllegalArgumentException e) {
+            // Verificação se a exceção lançada é a esperada.
+            assertEquals("Por favor, coloque um texto apresentando e " +
+                    "descrevendo a ação.", e.getMessage());
+            System.out.println("Por favor, coloque um texto apresentando e descrevendo a ação.");
         }
     }
 
     //Teste 3: Postagem com Valor Inválido (Número negativo)
-
     @Test
     public void PostarAcaoNaoPresencialValor1Test() {
-
-        //Declaração dos valores:
-
         String dataInicioString = "22-12-2022";
         String dataFinalString = "31-12-2022";
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
 
-        String valorTotal = "-3600"; //String com o valor que será inserido, dessa vez em inválido.
-        //String valorTotal = "3600"; //Descomentar caso queira que o teste passe.
+        String valorTotal = "-3600"; //String com o valor que será inserido, dessa vez inválido.
 
         try {
-
-            //Convertendo data inicial e final para Date:
 
             Date dataInicio = format.parse(dataInicioString);
             Date dataFinal = format.parse(dataFinalString);
 
-
-            //Checando se to que tem em 'valorTotal' é válido:
-
+            //Checando se o que tem em 'valorTotal' é válido:
             int valor = Integer.parseInt(valorTotal);
             if (valor <= 0) {
                 throw new IllegalArgumentException("Por favor, insira um valor válido para arrecadar.");
             }
 
             //Criando nova postagem Nao Presencial:
-
-            PostagemNaoPresencial pn = new PostagemNaoPresencial("Ola, queria pedir ajuda para pagar uma cirurgia do meu " +
-                    "cachorro", valorTotal, dataInicio, dataFinal);
+            PostagemNaoPresencial pn = new PostagemNaoPresencial("Ola, queria pedir ajuda " +
+                    "para pagar uma cirurgia do meu cachorro", valorTotal, dataInicio, dataFinal);
             PostagemNaoPresencialRepositorio pnr = new PostagemNaoPresencialRepositorio();
             Boolean addPostagemNaoPresencial = pnr.addPostagemNaoPresencial(pn);
             assertTrue(addPostagemNaoPresencial);
         } catch (ParseException e) {
             throw new RuntimeException(e);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Por favor, insira um valor válido para arrecadar.", e.getMessage());
+            System.out.println("Por favor, insira um valor válido para arrecadar.");
         }
     }
 
     //Teste 4: Postagem com Valor em Branco
-
     @Test
     public void PostarAcaoNaoPresencialValor2Test() {
-
-        //Declaração dos valores:
-
         String dataInicioString = "22-12-2022";
         String dataFinalString = "31-12-2022";
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
-
         String valorTotal = ""; //String com o valor que será inserido, dessa vez vazio.
-        //String valorTotal = "3600"; //Descomentar caso queira que o teste passe.
-
         try {
-
-            //Convertendo data inicial e final para Date:
-
             Date dataInicio = format.parse(dataInicioString);
             Date dataFinal = format.parse(dataFinalString);
 
-
-            //Checando se to que tem em 'valorTotal' está vazio:
-
+            //Checando se o que tem em 'valorTotal' está vazio:
             if (valorTotal.isEmpty()) {
                 throw new IllegalArgumentException("Por favor, insira um valor a ser arrecadado.");
             }
 
             //Criando nova postagem Nao Presencial:
-
             PostagemNaoPresencial pn = new PostagemNaoPresencial("Ola, queria pedir ajuda para pagar uma cirurgia do meu " +
                     "cachorro", valorTotal, dataInicio, dataFinal);
             PostagemNaoPresencialRepositorio pnr = new PostagemNaoPresencialRepositorio();
@@ -164,30 +142,24 @@ public class PostarAcaoNaoPresencial {
             assertTrue(addPostagemNaoPresencial);
         } catch (ParseException e) {
             throw new RuntimeException(e);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Por favor, insira um valor a ser arrecadado.", e.getMessage());
+            System.out.println("Por favor, insira um valor a ser arrecadado.");
         }
     }
 
     //Teste 5: Postagem com Data errada.
-
     @Test
     public void PostarAcaoNaoPresencialDatasTest() {
-        //Declaração dos valores:
-
-        /* String dataInicioString = "22-02-2023";
-        String dataFinalString = "22-12-2023"; //Descomentar caso queira que o teste passe. */
-
-        /*String dataInicioString = "22-02-2022";
-        String dataFinalString = "22-12-2023"; //Descomentar para ver erro 'data inicial anterior a data atual'.*/
+        //String dataInicioString = "22-02-2022";
+        //String dataFinalString = "22-12-2023"; //Erro 'data inicial anterior a data atual'.
 
         String dataInicioString = "22-03-2023";
-        String dataFinalString = "22-02-2023";
+        String dataFinalString = "22-02-2023"; //Erro 'data final anterior a data inicial'.
 
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
 
         try {
-
-            //Convertendo data inicial e final para Date:
-
             Date dataInicio = format.parse(dataInicioString);
             Date dataFinal = format.parse(dataFinalString);
 
@@ -202,17 +174,24 @@ public class PostarAcaoNaoPresencial {
             }
 
             //Criando nova postagem Nao Presencial:
-
             PostagemNaoPresencial pn = new PostagemNaoPresencial("Ola, queria pedir ajuda para pagar uma " +
-                    "cirurgia do meu cachorro","3600", dataInicio, dataFinal);
+                    "cirurgia do meu cachorro", "3600", dataInicio, dataFinal);
             PostagemNaoPresencialRepositorio pnr = new PostagemNaoPresencialRepositorio();
             Boolean addPostagemNaoPresencial = pnr.addPostagemNaoPresencial(pn);
             assertTrue(addPostagemNaoPresencial);
         } catch (ParseException e) {
             throw new RuntimeException(e);
+        } catch (IllegalArgumentException e) {
+
+            /* Verificando qual das duas exceções de data ocorreu (inicial anterior a atual
+            ou final anterior a inicial) e vendo a mensagem de exceção correta. */
+
+            if (!e.getMessage().equals("A data final não pode ser anterior a data inicial.") &&
+                    !e.getMessage().equals("A data inicial não pode ser anterior a data atual.")) {
+                throw e;
+            }
         }
     }
-
 }
 
 
--- src/main/teste/PostarAcaoPresencial.java
+++ src/main/teste/PostarAcaoPresencial.java
@@ -12,32 +12,36 @@ import static org.junit.Assert.*;
 
 public class PostarAcaoPresencial {
 
-    /* @Teste no. 1:
-     * 1. Declarar os valores que serão cadastrados na ação (Texto, Data, local).
-     * 2. Converter a data para o formato Date. Ela será salva no formato String.
-     * 3. Criação da postagemPresencial.
-     * */
-
     // Teste 1: Postagem de Ação (Presencial)
     @Test
     public void PostarAcaoPresencialTest() {
-        //String com a  Data:
-        String dateString = "22-12-2022"; //Data está salva em String na variável 'dateString'
+        // String com a data da ação presencial, armazenada na variável 'dateString'
+        String dateString = "22-12-2022";
+
+        // Instância da classe SimpleDateFormat, utilizada para formatar a data de String para Date
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
 
-        //Criação da Postagem:
         try {
-            Date date = format.parse(dateString); //Conversão da data em formato String para Date
+            // Conversão da data em formato String para Date
+            Date date = format.parse(dateString);
 
-            //Passagem dos parâmetros da postagem:
-            Postagem p = new Postagem("Ola, queria sugerir um grupo para distribuir " +
-                    "sopa na praca para moradores de rua", date, "Praça do Derby");
+            // Instância da classe Postagem, passando como parâmetros:
+            // - A descrição da ação
+            // - A data da ação (já convertida para o tipo Date)
+            // - O local da ação
+            Postagem p = new Postagem("Ola, queria sugerir um grupo para distribuir sopa na praca " +
+                    "para moradores de rua", date, "Praça do Derby");
 
-            //Continuação da criação da postagem:
+            // Instância da classe PostagemRepositorio, responsável por adicionar a postagem
             PostagemRepositorio pr = new PostagemRepositorio();
+
+            // Adicionando a postagem e armazenando o resultado (se postou ou não) na variável 'postou'
             Boolean postou = pr.addPostagem(p);
+
+            // Verificando se a postagem foi adicionada com sucesso
             assertTrue(postou);
         } catch (ParseException e) {
+            // Caso haja algum erro na conversão da data, é lançada uma exceção do tipo RuntimeException
             throw new RuntimeException(e);
         }
     }
@@ -45,26 +49,43 @@ public class PostarAcaoPresencial {
     // Teste 2: Mensagem em branco.
     @Test
     public void PostarAcaoPresencialTextTest() {
+        //String com a data da ação:
         String dateString = "22-12-2022";
+        //Formato da data, nesse caso é "dd-MM-yyyy":
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+
         try {
+            //Conversão da data em formato String para Date:
             Date date = format.parse(dateString);
-            String texto = ""; //String com o texto que será inserido, dessa vez em branco.
+
+            //String com o texto da ação, inicialmente em branco:
+            String texto = "";
+            //Verificação se o texto da ação está vazio:
             if (texto.isEmpty()) {
+                //Se estiver vazio, lança uma exceção com mensagem abaixo.
                 throw new IllegalArgumentException("Por favor, coloque um texto apresentando e descrevendo a ação.");
             }
-            //Ao invés de inserir a string diretamente, coloca-se a String 'texto' declarada acima:
+
+            //Ao invés de inserir a string diretamente, coloca-se a String 'texto' declarada acima.
             Postagem p = new Postagem(texto, date, "Praça do Derby");
+            //Instanciamento do repositório de postagens:
             PostagemRepositorio pr = new PostagemRepositorio();
+            //Adição da postagem ao repositório:
             Boolean postou = pr.addPostagem(p);
+            //Verificação se a postagem foi adicionada com sucesso:
             assertTrue(postou);
         } catch (IllegalArgumentException e) {
+            //Se lançar uma exceção com a mensagem "Por favor, coloque um texto apresentando e descrevendo a ação.":
             if (e.getMessage().equals("Por favor, coloque um texto apresentando e descrevendo a ação.")) {
+                System.out.println("Por favor, coloque um texto apresentando e descrevendo a ação.");
+                //Retorna sem fazer mais nada
                 return;
             } else {
+                //Se não, lança a exceção:
                 throw e;
             }
         } catch (ParseException e) {
+            //Lançamento da exceção "ParseException":
             throw new RuntimeException(e);
         }
     }
@@ -79,10 +100,10 @@ public class PostarAcaoPresencial {
                 throw new IllegalArgumentException("Por favor, insira uma data.");
             } catch (IllegalArgumentException e) {
                 assertEquals("Por favor, insira uma data.", e.getMessage());
+                System.out.println("Por favor, insira uma data.");
             }
             return;
-        }
-        try {
+        } try {
             Date date = format.parse(dateString);
             Postagem p = new Postagem("Ola, queria sugerir um grupo para distribuir sopa na " +
                     "praca para moradores de rua", date, "Praça do Derby");
@@ -97,26 +118,42 @@ public class PostarAcaoPresencial {
     //Teste 4: Data inválida.
     @Test
     public void PostarAcaoPresencialDataInvalidaTest() {
-        //Cria uma instância da class Util.date. Guarda a data atual na variável 'DataAtual'.
-        Date DataAtual = new Date();
+        //Cria uma instância da classe Date para guardar a data atual na variável dataAtual.
+        Date dataAtual = new Date();
+        //Cria uma instância da classe SimpleDateFormat para formatar a data.
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+        //Variável dateString armazena a data a ser comparada com a data atual.
         String dateString = "22-12-2022";
-        boolean thrown = false;
         try {
+            //Cria uma instância da classe Date para guardar a data da postagem na variável postDate.
             Date postDate = format.parse(dateString);
-            if (postDate.before(DataAtual)) {
-                thrown = true;
+            //Verifica se a data da postagem é anterior a data atual.
+            if (postDate.before(dataAtual)) {
+                //Lança uma exceção com uma mensagem de erro caso a data seja inválida.
                 throw new IllegalArgumentException("Data inválida. Por favor, insira uma data posterior a hoje.");
             }
+            //Cria uma instância da classe Postagem com as informações da postagem.
             Postagem p = new Postagem("Ola, queria sugerir um grupo para distribuir sopa na praca " +
                     "para moradores de rua", postDate, "Praça do Derby");
+            //Cria uma instância da classe PostagemRepositorio para adicionar a postagem.
             PostagemRepositorio pr = new PostagemRepositorio();
+            //Chama o método addPostagem e armazena o resultado na variável postou.
             Boolean postou = pr.addPostagem(p);
+            //Verifica se a postagem foi adicionada com sucesso.
             assertTrue(postou);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
         } catch (IllegalArgumentException e) {
-            assertTrue(thrown);
+            //Trata a exceção lançada na verificação da data.
+            if (e.getMessage().equals("Data inválida. Por favor, insira uma data posterior a hoje.")) {
+                System.out.println("Data inválida. Por favor, insira uma data posterior a hoje.");
+                //Encerra o teste caso a data seja inválida.
+                return;
+            } else {
+                //Lança a exceção caso haja algum outro erro.
+                throw e;
+            }
+        } catch (ParseException e) {
+            //Trata a exceção lançada na conversão da data.
+            throw new RuntimeException(e);
         }
     }
 
@@ -127,7 +164,7 @@ public class PostarAcaoPresencial {
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
         try {
             Date date = format.parse(dateString);
-            String local = ""; //String do local da ação, dessa vez em branco.
+            String local = "";
             if (local.isEmpty()) {
                 throw new IllegalArgumentException("Por favor, insira um local para a ação presencial.");
             }
@@ -138,6 +175,7 @@ public class PostarAcaoPresencial {
             assertTrue(postou);
         } catch (IllegalArgumentException e) {
             assertEquals("Por favor, insira um local para a ação presencial.", e.getMessage());
+            System.out.println("Por favor, insira um local para a ação presencial.");
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
